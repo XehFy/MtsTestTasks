34150c8dfb2844559ea76e74ca0d049c2ad4498f
XehFy <94968044+XehFy@users.noreply.github.com>
2022-10-15 18:14:28 +0300

    Update Program.cs

--- Task_5/Program.cs
+++ Task_5/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 
-// Придумал только запустить задачу очистки экрана и вывода слона асинхронно, так, чтобы она выполнилась после вывода мухи
-
+//  первый пришедший в голову вариант
 class Program
 {
 	static void Main(string[] args)
@@ -20,4 +19,27 @@ class Program
 			await Task.Delay(10);
 		});
 	}
+// Другой вариант, перенаправить вывод из консоли в stringWriter 
+// до появления слова "Муха", затем заменить первое вхождение мухи на слона, 
+// вернуть стандартный вывод, вывести изменённое содержимое stringWriter. 
+        static void TransformToElephant() 
+        {       //... write your code here  } 
+            var std =  Console.Out; 
+            System.IO.StringWriter writer = new System.IO.StringWriter(); 
+            Console.SetOut(writer); 
+            new Thread(() => { 
+                while (!writer.ToString().Contains("Муха")) 
+                { 
+                    Thread.Sleep(1000); 
+                } 
+                string sOut = writer.ToString(); 
+                //sOut = sOut.Replace("Муха", "Слон"); 
+                Console.SetOut(std); 
+                string Replace = "Слон";
+                string Find = "Муха";
+                int Place = Source.IndexOf(Find);
+                Source.Remove(Place, Find.Length).Insert(Place, Replace);
+                Console.WriteLine(sOut); 
+            }).Start(); 
+        }
 }
